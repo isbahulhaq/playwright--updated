9356430076dae64d99620742f9d85897a19a9a7b
isbahulhaq <167408032+isbahulhaq@users.noreply.github.com>
2025-07-10 19:09:28 +0530

    Create Scrpit.py

---
+++ Scrpit.py
@@ -0,0 +1,9 @@
+!playwright install-deps
+!sudo apt-get install -y libnss3 \
+                     libnspr4 \
+                     libatk-bridge2.0-0 \
+                     libdrm2 \
+                     libxkbcommon0 \
+                     libatspi2.0-0 \
+                     libgbm1 \
+                     libasound2
