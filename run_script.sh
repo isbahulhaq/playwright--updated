2123772405fcff2f4409f1cdab4eda73ae81e521
isbahulhaq <167408032+isbahulhaq@users.noreply.github.com>
2025-07-10 19:13:03 +0530

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the GitHub repository
+git clone https://github.com/AmitKehrwal/Playwright-Updated.git
+
+# Navigate to the repository directory
+cd Playwright-Updated
+
+# Run the Python script
+python Script.py
